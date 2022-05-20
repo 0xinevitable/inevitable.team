1ba2aef2a7439d4276b7910c527b8c4fc88242b9
Junho Yeo <i@junho.io>
2022-05-20 20:20:14 +0900

    Initialize project

---
+++ src/landing/LandingPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LandingPage = () => {
+  return <Container>Inevt</Container>;
+};
+
+export default LandingPage;
+
+const Container = styled.div``;
