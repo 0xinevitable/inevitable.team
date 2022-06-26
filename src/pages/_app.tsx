486b79ce0df76eac1f9dd83338d3c65cc2902cac
Junho Yeo <i@junho.io>
2022-06-27 05:13:06 +0900

    Move stylesheets to `Document` and add `collectStyles`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @next/next/no-page-custom-font */
-import Head from 'next/head';
 import React from 'react';
 
 import { GlobalStyle } from '@/styles/GlobalStyle';
@@ -7,18 +5,6 @@ import { GlobalStyle } from '@/styles/GlobalStyle';
 function MyApp({ Component, pageProps }) {
   return (
     <React.Fragment>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </React.Fragment>
---
+++ src/pages/_document.tsx
@@ -0,0 +1,57 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+import { GlobalStyle } from '@/styles/GlobalStyle';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()] as any,
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <GlobalStyle />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
