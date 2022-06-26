0bcbae56c9b607c40c63c483de027df06afee00d
Junho Yeo <i@junho.io>
2022-06-27 05:04:22 +0900

    Update dependencies

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 import normalize from 'styled-normalize';
 
-export const GlobalStyle = createGlobalStyle`
+export const GlobalStyle: any = createGlobalStyle`
   ${normalize}
 
   * {
